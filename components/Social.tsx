3523bfd45dff61c1ce7daaebce5765a42be347ed
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-08-16 20:31:00 -0500

    .

--- components/Social.tsx
+++ components/Social.tsx
@@ -9,8 +9,8 @@ interface SocialProps {
 
 export const Social: FC<SocialProps> = ({ image, url, text }) => {
   return (
-    <a href={typeof url === 'undefined' ? '' : url} target='_blank' className={`${typeof url === 'undefined' ? 'cursor-default' : 'cursor-pointer'}`}>
-      <div className='grid grid-cols-[max-content_auto] place-items-center bg-[#282828] rounded-[10px] px-[4px] hover:bg-[#212121]'>
+    <a href={typeof url === 'undefined' ? '' : url} target='_blank' rel='noreferrer' className={`${typeof url === 'undefined' ? 'cursor-default' : 'cursor-pointer'}`}>
+      <div className='grid grid-cols-[max-content_auto] place-items-center bg-[#282828] rounded-[10px] py-[4px] px-[4px] hover:bg-[#212121]'>
         <div className='w-[30px] h-[30px] md:w-[50px] md:h-[50px] block place-items-center'>
           <Image src={image} layout='responsive' alt='' />
         </div>
