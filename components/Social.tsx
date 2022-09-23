b021d8f89476e8759d8c90025057d63ab123f4b6
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-09-23 16:07:22 -0500

    .

--- components/Social.tsx
+++ components/Social.tsx
@@ -10,7 +10,7 @@ interface SocialProps {
 export const Social: FC<SocialProps> = ({ image, url, text }) => {
   if (url)
     return (
-      <a href={url} target='_blank' rel='noreferrer' className='cursor-pointer'>
+      <a href={url} target='_blank' rel='noopener noreferrer' className='cursor-pointer'>
         <div className='grid grid-cols-[max-content_auto] place-items-center bg-[#282828] rounded-[10px] py-[4px] px-[4px] hover:bg-[#212121]'>
           <div className='w-[30px] h-[30px] md:w-[50px] md:h-[50px] block place-items-center'>
             <Image src={image} layout='responsive' alt='' />
--- next.config.js
+++ next.config.js
@@ -1,13 +1,33 @@
 /** @type {import('next').NextConfig} */
 
 const path = require('path');
+const loaderUtils = require('loader-utils');
+const MangleCssClassPlugin = require('mangle-css-class-webpack-plugin');
 
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
+  },
+  webpack(config, { dev }) {
+    const rules = config.module.rules.find(rule => typeof rule.oneOf === 'object').oneOf.filter(rule => Array.isArray(rule.use));
+
+    config.plugins.push(
+      new MangleCssClassPlugin({
+        classNameRegExp: '((hover|focus|xs|md|sm|lg|xl)[\\\\]*:)*tw-[a-z_-][a-zA-Z0-9_-]*',
+        ignorePrefixRegExp: '((hover|focus|xs|md|sm|lg|xl)[\\\\]*:)*',
+        log: true
+      })
+    );
+
+    if (!dev)
+      rules.forEach(rule => {
+        rule.use.forEach(moduleLoader => {
+          if (moduleLoader.loader?.includes('css-loader') && !moduleLoader.loader?.includes('postcss-loader')) moduleLoader.options.modules.getLocalIdent = (context, _, exportName) => loaderUtils.getHashDigest(Buffer.from(`filePath:${path.relative(context.rootContext, context.resourcePath).replace(/\\+/g, '/')}#className:${exportName}`), 'md4', 'base64', 6).replace(/^(-?\d|--)/, '_$1');
+        });
+      });
+
+    return config;
   }
 };
-
-module.exports = nextConfig;
