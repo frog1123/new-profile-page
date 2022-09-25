1f70e7445f885f83d4c0fa302dabdd83d5b597c9
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-09-25 13:39:21 -0500

    more performance

--- components/FeaturedVideo.tsx
+++ components/FeaturedVideo.tsx
@@ -8,7 +8,8 @@ export const FeaturedVideo: FC = () => {
           FEATURED VIDEO
         </h1>
         <div className='tw-aspect-video tw-w-[200px] sm:tw-w-[400px] md:tw-w-[600px] lg:tw-w-[800px]'>
-          <iframe src='https://www.youtube.com/embed/G6ImwlyveCY' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen id='featured-video' className='gradient-border tw-disable-select tw-w-[100%] tw-h-[100%]'></iframe>
+          {/* <iframe src='https://www.youtube.com/embed/G6ImwlyveCY' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen id='featured-video' className='gradient-border tw-disable-select tw-w-[100%] tw-h-[100%]'></iframe> */}
+          <iframe id='featured-video' className='gradient-border tw-disable-select tw-w-[100%] tw-h-[100%]' title='featured-video' aria-hidden='true' frameBorder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowFullScreen srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:#d9ed92;}</style><a href=https://www.youtube.com/embed/G6ImwlyveCY/?autoplay=1><img src=https://img.youtube.com/vi/G6ImwlyveCY/hqdefault.jpg alt='featured video'><span>▶</span></a>`}></iframe>
         </div>
       </div>
     </div>
