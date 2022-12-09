7eecde0b7004f5b1fb5e8d49f3ef402c6348519d
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-12-09 17:39:27 -0600

    +

--- components/DownloadContainer.tsx
+++ components/DownloadContainer.tsx
@@ -4,17 +4,19 @@ import styles from '@styles/DownloadContainer.module.scss';
 interface DownloadContainerProps {
   name: string;
   size: string;
-  downloadUrl: string;
+  downloadUrl?: string;
+  text?: string;
+  externalUrl?: string;
 }
 
-export const DownloadContainer: FC<DownloadContainerProps> = ({ name, size, downloadUrl }) => {
+export const DownloadContainer: FC<DownloadContainerProps> = ({ name, size, downloadUrl, text, externalUrl }) => {
   return (
-    <div className='tw-grid md:tw-grid-cols-[7fr_1fr_max-content] tw-mx-[40px] tw-place-items-center'>
+    <div className='tw-grid md:tw-grid-cols-[7fr_1fr_2fr] tw-mx-[40px] tw-place-items-center'>
       <h1 className='gradient-text md:tw-mr-auto'>{name}</h1>
       <h1 className='tw-text-white'>{size}</h1>
-      <a href={downloadUrl} download>
-        <button className={`dag-gradient tw-p-[10px] tw-cursor-pointer ${styles['download-button']}`}>
-          <h1 className='tw-text-white'>download</h1>
+      <a href={externalUrl ? externalUrl : downloadUrl} download>
+        <button className={`dag-gradient tw-p-[10px] tw-w-[100%] tw-cursor-pointer ${styles['download-button']}`}>
+          <h1 className='tw-text-white'>{text ? text : 'download'}</h1>
         </button>
       </a>
     </div>
--- pages/downloads.tsx
+++ pages/downloads.tsx
@@ -33,12 +33,15 @@ const Downloads: NextPage = () => {
         <div className='frog-hidden tw-bg-[#333333] tw-flex tw-justify-center tw-px-[6px] tw-pt-[15px] tw-pb-[10px] tw-rounded-[10px] md:tw-rounded-[20px] tw-border-[#b5e48c] tw-border-[4px_0_0_0]'>
           <div className='tw-grid tw-grid-flow-row tw-place-items-center tw-w-[100%]'>
             <h1 className='gradient-text tw-text-[24px] tw-font-bold'>DOWNLOADS</h1>
-            <div className='tw-w-[100%] tw-grid tw-gap-[10px]'>
+            <div className='tw-w-[100%] tw-grid tw-gap-[10px] tw-pt-[2px]'>
+              <h1 className='gradient-text tw-text-[18px] tw-font-bold tw-m-auto'>github stellar theme</h1>
               <DownloadContainer name='github-stellar-theme-v2@1.0.0.zip' size='19.6KB' downloadUrl='/downloads/github-stellar-theme-v2@1.0.0.zip' />
               <DownloadContainer name='github-stellar-theme-v2@1.0.1.zip' size='20.5KB' downloadUrl='/downloads/github-stellar-theme-v2@1.0.1.zip' />
               <DownloadContainer name='github-stellar-theme-v2@1.0.2.zip' size='20.3KB' downloadUrl='/downloads/github-stellar-theme-v2@1.0.2.zip' />
               <DownloadContainer name='github-stellar-theme-v2@1.0.3.zip' size='23.8KB' downloadUrl='/downloads/github-stellar-theme-v2@1.0.3.zip' />
               <DownloadContainer name='github-stellar-theme-v2@1.0.4.zip' size='25.5KB' downloadUrl='/downloads/github-stellar-theme-v2@1.0.4.zip' />
+              <h1 className='gradient-text tw-text-[18px] tw-font-bold tw-m-auto'>frogtopia x</h1>
+              <DownloadContainer name='frogtopia_x_100days' size='60.89MB' text='mediafire' externalUrl='https://www.mediafire.com/file/ub54hlep9ea29rm/frogtopia_x_100days.zip/' />
             </div>
           </div>
         </div>
